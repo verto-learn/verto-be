e8a12bc0ad390a51ececde1f8b01ad3c9dbc46fc
kanaee-cloud <arsalnaufal2007@gmail.com>
2025-11-10 13:00:35 +0700

    feat: add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import cookieParser from "cookie-parser";
 import router from './features/v1.routes';
+import cors from "cors";
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://verto-fe.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1", router);
@@ -14,5 +23,7 @@ app.get("/", (req, res) => {
     message: "Welcome to Verto API",
   });
 });
+
+app.use(errorHandler)
 
 export default app;
